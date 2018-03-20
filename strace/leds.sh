2ac07fccdbccfb8a9da153cc77621d9310f6bc07
Mathias Fredriksson <mafredri@gmail.com>
2018-03-20 18:54:37 +0200

    Add traces and straces

---
+++ strace/leds.sh
@@ -0,0 +1,88 @@
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-status_on ledctrl -status on
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-status_blink1 ledctrl -status blink1
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-status_blink2 ledctrl -status blink2
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-status_blink3 ledctrl -status blink3
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-status_blink4 ledctrl -status blink4
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-status_off ledctrl -status off
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-power_on ledctrl -power on
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-power_off ledctrl -power off
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-system_on ledctrl -system on
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-system_off ledctrl -system off
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-sleep_on ledctrl -sleep on
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-sleep_off ledctrl -sleep off
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-usb_on ledctrl -usb on
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-usb_blink1 ledctrl -usb blink1
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-usb_blink2 ledctrl -usb blink2
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-usb_blink3 ledctrl -usb blink3
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-usb_blink4 ledctrl -usb blink4
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-usb_off ledctrl -usb off
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-lan_on ledctrl -lan on
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-lan_off ledctrl -lan off
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-inspection_on ledctrl -inspection on
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-inspection_blink1 ledctrl -inspection blink1
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-inspection_blink2 ledctrl -inspection blink2
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-inspection_blink3 ledctrl -inspection blink3
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-inspection_blink4 ledctrl -inspection blink4
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-inspection_off ledctrl -inspection off
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-alert_on ledctrl -alert on
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-alert_blink1 ledctrl -alert blink1
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-alert_blink2 ledctrl -alert blink2
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-alert_blink3 ledctrl -alert blink3
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-alert_blink4 ledctrl -alert blink4
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-alert_off ledctrl -alert off
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-sas_on ledctrl -sas on
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-sas_blink1 ledctrl -sas blink1
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-sas_blink2 ledctrl -sas blink2
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-sas_blink3 ledctrl -sas blink3
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-sas_blink4 ledctrl -sas blink4
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-sas_off ledctrl -sas off
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-lan10g_on ledctrl -lan10g on
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-lan10g_blink1 ledctrl -lan10g blink1
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-lan10g_blink2 ledctrl -lan10g blink2
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-lan10g_blink3 ledctrl -lan10g blink3
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-lan10g_blink4 ledctrl -lan10g blink4
+
+../ltrace/ltrace -l libasapi.so -l libbuiltin.so -l libce5300drv.so -l libcedarview.so -l libcgi.so -l libcharset.so -l libdenlow.so -l libgeneral.so -l libgeneraldrv.so -l libhalgeneral.so -l libiconv.so -l libmarvell.so -l libnasman.so -l libndal.so -l libnhal.so -l libplugin.so -l libservice.so -l libwrapper.so -SCfi -n 2 -s 500 -o ledctrl-lan10g_off ledctrl -lan10g off
+
